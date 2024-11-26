fb5e6a1f46a188c960e22623c80315a3d3a45046
Alejandro López <ale64bit@gmail.com>
2024-11-26 15:06:16 +0300

    Highlight completed trainings

--- src/solve_preset_window.cc
+++ src/solve_preset_window.cc
@@ -647,6 +647,14 @@ static std::string time_challenge_label(Rank rank, int total, int fails) {
   return ss.str();
 }
 
+static std::string topic_training_label(Rank rank, int total, int fails) {
+  std::ostringstream ss;
+  ss << "<span ";
+  if (total > fails) ss << "weight=\"bold\" foreground=\"deepskyblue\"";
+  ss << ">" << rank_string(rank) << "</span>";
+  return ss.str();
+}
+
 SolvePresetWindow::SolvePresetWindow(AppContext& ctx) : Window(ctx) {
   gtk_window_set_title(GTK_WINDOW(window_), "Training");
   gtk_window_set_default_size(GTK_WINDOW(window_), 900, 550);
@@ -731,8 +739,10 @@ SolvePresetWindow::SolvePresetWindow(AppContext& ctx) : Window(ctx) {
 
         presets_.emplace_back(preset, std::make_pair(tag_id, Rank(rank)));
 
-        GtkWidget* preset_button =
-            gtk_button_new_with_label(rank_string(Rank(rank)));
+        GtkWidget* preset_label = gtk_label_new("");
+        gtk_label_set_justify(GTK_LABEL(preset_label), GTK_JUSTIFY_CENTER);
+        GtkWidget* preset_button = gtk_button_new();
+        gtk_button_set_child(GTK_BUTTON(preset_button), preset_label);
         g_object_set_data(G_OBJECT(preset_button), "preset_index",
                           (gpointer)(presets_.size() - 1));
         g_signal_connect(preset_button, "clicked",
@@ -797,17 +807,20 @@ void SolvePresetWindow::update_preset_buttons() {
 void SolvePresetWindow::update_time_challenge_button_label(
     GtkWidget* button, int tag_id, Rank rank,
     const std::map<Rank, std::pair<int, int>>& stats) {
+  GtkWidget* label = gtk_button_get_child(GTK_BUTTON(button));
+  assert(label != nullptr);
+  int total = 0;
+  int fails = 0;
+  if (auto it = stats.find(rank); it != stats.end()) {
+    total = it->second.first;
+    fails = it->second.second;
+  }
   if (tag_id == kTimeChallengePreset) {
-    GtkWidget* label = gtk_button_get_child(GTK_BUTTON(button));
-    assert(label != nullptr);
-    int total = 0;
-    int fails = 0;
-    if (auto it = stats.find(rank); it != stats.end()) {
-      total = it->second.first;
-      fails = it->second.second;
-    }
     gtk_label_set_markup(GTK_LABEL(label),
                          time_challenge_label(rank, total, fails).c_str());
+  } else {
+    gtk_label_set_markup(GTK_LABEL(label),
+                         topic_training_label(rank, total, fails).c_str());
   }
 }
 
